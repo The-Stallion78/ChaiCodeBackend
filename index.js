5e0028b8531e30e8468c5896a2d7b9ff82a47c04
The-Stallion78 <khizaerhayat@gmail.com>
2025-03-17 14:27:34 +0500

    added the github data api

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const PORT = 3000
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const githubData = {
+    name: 'Khizar Hayat',
+    username: 'khizarhayat12',
+    email: 'khizarhayt@gmail.com',
+    age: 21,
+    country: 'Pakistan',
+    profession: 'Software Engineer'
+};
 
 app.get('/', (request, response) => {
-  response.send('Hello Khizar!')
-})
+    response.send('Hello Khizar!');
+});
 
-app.get('/twitter', (request, response)=>{
-    response.send('khizarhayat12')
-})
+app.get('/gitData', (request, response) => {
+    response.json(githubData);
+});
 
-app.get('/login', (request, response)=>{
-    response.send('<h1>Please Login to Chai Aur Code</h1>')
-})
+app.get('/twitter', (request, response) => {
+    response.send('khizarhayat12');
+});
 
-app.get('/signup', (request, response)=>{
-    response.send('<h2>Please Sign In to your Account</h2>')
-})
+app.get('/login', (request, response) => {
+    response.send('<h1>Please Login to Chai Aur Code</h1>');
+});
 
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
-}) 
+app.get('/signup', (request, response) => {
+    response.send('<h2>Please Sign In to your Account</h2>');
+});
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`);
+});
